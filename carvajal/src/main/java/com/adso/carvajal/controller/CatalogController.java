4772098498b348396749cab1ce3ececb095054a1
Dana <arrubladana@gmail.com>
2026-06-18 09:30:45 -0500

    feat: try-catch controller

--- carvajal/src/main/java/com/adso/carvajal/controller/CatalogController.java
+++ carvajal/src/main/java/com/adso/carvajal/controller/CatalogController.java
@@ -2,11 +2,14 @@ package com.adso.carvajal.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.adso.carvajal.dto.HttpGlobalResponseDTO;
+import com.adso.carvajal.dto.MessageResponseDTO;
 import com.adso.carvajal.service.CatalogService;
 
 import lombok.RequiredArgsConstructor;
@@ -19,7 +22,13 @@ public class CatalogController {
     private final CatalogService catalogService;
 
     @GetMapping("/list")
-    public HttpGlobalResponseDTO<List<Object[]>> getCatalogProducts() {
-        return catalogService.getCatalogProducts();
+    public ResponseEntity<HttpGlobalResponseDTO<List<Object[]>>> getCatalogProducts() {
+        try {
+            HttpGlobalResponseDTO<List<Object[]>> response = catalogService.getCatalogProducts();
+            return ResponseEntity.status(HttpStatus.CREATED).body(response);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
     }
 }
--- carvajal/src/main/java/com/adso/carvajal/controller/WishListController.java
+++ carvajal/src/main/java/com/adso/carvajal/controller/WishListController.java
@@ -15,10 +15,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PutMapping;
 
 
@@ -32,23 +33,48 @@ public class WishListController {
     private final WishListService wishListService;
 
     @PostMapping("/create-wishList")
-    public MessageResponseDTO createWishList(@RequestBody WishListCreateDTO wishListCreateDTO) {
-
-        return wishListService.createWishList(wishListCreateDTO);
+    public ResponseEntity<MessageResponseDTO> createWishList(@RequestBody WishListCreateDTO wishListCreateDTO) {
+        try {
+            MessageResponseDTO response = wishListService.createWishList(wishListCreateDTO);
+            return ResponseEntity.status(HttpStatus.CREATED).body(response);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
     }
     
     @GetMapping("/list/{id}")
-    public HttpGlobalResponseDTO<List<Object[]>> getWishList(@PathVariable Long id) {
-        return wishListService.getWishList(id);
+    public ResponseEntity<HttpGlobalResponseDTO<List<Object[]>>> getWishList(@PathVariable Long id) {
+        try {
+            HttpGlobalResponseDTO<List<Object[]>> response = wishListService.getWishList(id);
+            return ResponseEntity.status(HttpStatus.CREATED).body(response);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+
     }
     
     @DeleteMapping("/delete/{id}")
-    public MessageResponseDTO deleteWishList(@PathVariable Long id){
-        return wishListService.deleteWishList(id);
+    public ResponseEntity<MessageResponseDTO> deleteWishList(@PathVariable Long id){
+        try {
+            MessageResponseDTO response = wishListService.deleteWishList(id);
+            return ResponseEntity.status(HttpStatus.CREATED).body(response);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
     }
 
     @PutMapping("path/{id}")
-    public HttpGlobalResponseDTO<List<Object[]>> putWishList(@PathVariable Long id, @RequestBody WishListUpdateresponseDTO wUpdateresponseDTO) {
-        return wishListService.putWishList(id,wUpdateresponseDTO);
+    public ResponseEntity<HttpGlobalResponseDTO<List<Object[]>>> putWishList(@PathVariable Long id, @RequestBody WishListUpdateresponseDTO wUpdateresponseDTO) {
+        try {
+            HttpGlobalResponseDTO<List<Object[]>> response = wishListService.putWishList(id,wUpdateresponseDTO);
+            return ResponseEntity.status(HttpStatus.CREATED).body(response);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+        
     }
 }
